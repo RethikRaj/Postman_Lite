49bb67d2fa2f4c65e9c2cb7faa3a60f4383a0cae
RethikRaj <rethikrajrr@gmail.com>
2026-07-15 20:11:19 +0530

    FIX: chnaged file name

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { executeProxyRequest } from "./proxyExceutor.js";
+import { executeProxyRequest } from "./proxyExecutor.js";
 
 export const app = express();
 
